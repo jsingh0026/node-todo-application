a2a1b9482a267f1eb7fb28536ff65ca2b2f8bf90
jsingh0026 <jass.singh0026@gmail.com>
2020-04-08 00:57:07 +0530

    - server update -

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const http = require('http');
 const app = require('./app');
 
-const server = http.createServer(app);
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-server.listen(process.env.PORT || 3000);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+    const server = http.createServer(app);
+    server.listen(process.env.PORT || 3000);
+
+  console.log(`Worker ${process.pid} started`);
+}
